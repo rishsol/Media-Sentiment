b4654b43c45fb5cfc6edaa3af50658017c5c88c8
rishsol <rishabsolanki09@gmail.com>
2020-06-02 18:10:13 -0400

    cleanup

--- f.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-r = requests.get('https://www.benzinga.com/').text
-soup = BeautifulSoup(r, 'html.parser')
-print(soup.findAll('div', {'class': 'views-field-title'}))
